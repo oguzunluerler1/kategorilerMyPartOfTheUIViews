12c01c1793f7eba58a1c238749eb4bf04bc182fd
Oğuz Ünlüerler <oguzunluerler1@gmail.com>
2022-07-13 01:03:28 +0300

    deneme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:oguz_views/screens/kayitlikartlarim.dart';
-import 'package:oguz_views/screens/kullanicisepet.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
